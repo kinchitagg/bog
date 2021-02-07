ecdad3b1444341b57c47b78b83e50f3a9f5ba13f
kinchitagg <kinchitaggarwal1243@gmail.com>
2021-02-07 22:31:29 +0530

    Final commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,16 @@ import 'package:klimatic/Provider/cityProvider.dart';
 import 'package:klimatic/screen/home/weatherForcast.dart';
 import 'package:provider/provider.dart';
 
-void main(){
+
+void main() {
   runApp(new MaterialApp(
     title: 'Klimatic',
-    home: ChangeNotifierProvider(
-      create: (context)=> CityProvider(),
-      child: WeatherForcast()),
-  ));
+    home: 
+      ChangeNotifierProvider(
+        create: (context) => CityProvider(),
+         child: WeatherForcast()),
+      ),
+      
+    
+  );
 }
